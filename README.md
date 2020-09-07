243c757596f3c9c47f949fe050805f36b604a111
VORTX\ala <ala@vortx.com.br>
2020-09-07 20:34:00 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Agenda-Petshop
+
+Esse repositório é um clone do curso Iniciando com GraphQL disponível na [Alura](https://cursos.alura.com.br).
+
+## REPOSITÓRIO OFICIAL: https://github.com/juunegreiros/agenda-petshop-front
+
+## Instalação
+
+Dentro da pasta, faça a instalação do projeto:
+
+```
+  npm install
+```
+
+E, por fim, rode o projeto:
+
+```
+  npm start
+```
---
+++ src/api/clientes.js
@@ -0,0 +1,47 @@
+import { api, opcoesFetch } from './config'
+
+const listarClientes = () => 
+  fetch('http://localhost:4000', opcoesFetch('{clientes { id nome cpf }}'))
+    .then(resposta => resposta.json())
+    .then(dados => dados.data.clientes)
+
+const buscarClientePorId = id => 
+  fetch('http://localhost:4000', opcoesFetch(`{cliente (id: ${id})  { nome cpf }}`))
+    .then(resposta => resposta.json())
+    .then(dados => dados.data.cliente)
+
+const adicionarCliente = cliente => 
+  fetch('http://localhost:4000', opcoesFetch(`mutation {
+    adicionarCliente(nome: "${cliente.nome}", cpf: "${cliente.cpf}") {
+      id
+      nome
+    }
+  }`))
+    .then(resposta => resposta.json())
+    .then(dados => dados.data.cliente)
+
+const alterarCliente = (id, cliente) =>
+  fetch('http://localhost:4000', opcoesFetch(`
+    mutation {
+      atualizarCliente(id: ${id}, nome: "${cliente.nome}", cpf: "${cliente.cpf}") {
+        nome
+      }
+    }
+  `))
+    .then(resposta => resposta.json())
+    .then(dados => dados.data)
+
+const removerCliente = id => 
+  fetch('http://localhost:4000', opcoesFetch(`
+  mutation {
+    deletarCliente(id: ${id})
+  }
+  `))
+
+export default {
+  listarClientes,
+  buscarClientePorId,
+  adicionarCliente,
+  alterarCliente,
+  removerCliente
+}
